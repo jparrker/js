56472bcc2246dfc26d6c9f1dcbb78100261a9315
jparrker <jasonkk.parker@gmail.com>
2022-04-23 19:57:13 -0400

    solution

---
+++ quadraticCoeff.js
@@ -0,0 +1,6 @@
+function quadratic(x1, x2) {
+  a = 1;
+  b = x2 * -1 + x1 * -1;
+  c = x1 * x2;
+  return [a, b, c];
+}
---
+++ room_objects.js
@@ -0,0 +1,17 @@
+var rooms = {
+  room1: {
+    name: "room1",
+    description: "foo",
+    completed: "bar",
+  },
+  room2: {
+    name: "room1",
+    description: "foo",
+    completed: "bar",
+  },
+  room3: {
+    name: "room1",
+    description: "foo",
+    completed: "bar",
+  },
+};
