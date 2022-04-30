38e45284070346376c662774014579f3108c3606
jparrker <jasonkk.parker@gmail.com>
2022-04-30 17:11:28 -0400

    initial set up

---
+++ character_inventory.js
@@ -0,0 +1,30 @@
+/* Character inventory
+Improve the example RPG to add character inventory management according to the following rules:
+
+A character's inventory contains a number of gold and a number of keys.
+
+Each character begins with 10 gold and 1 key.
+
+The character description must show the inventory state.
+
+When a character slays another character, the victim's inventory goes to its vanquisher.
+*/
+
+class Character {
+  constructor(name, health, strength) {
+    this.property1 = param1;
+    this.property2 = param2;
+    // ...
+  }
+  method1(/* ... */) {
+    // ...
+  }
+  method2(/* ... */) {
+    // ...
+  }
+  // ...
+}
+
+const myObject = new MyClass(arg1, arg2, ...);
+myObject.method1(/* ... */);
+
