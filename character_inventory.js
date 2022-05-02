0e2787825271ad64f2b6070fc88bda8fc072b20a
jparrker <jasonkk.parker@gmail.com>
2022-05-02 19:51:23 -0400

    finished attack logic

--- character_inventory.js
+++ character_inventory.js
@@ -13,10 +13,11 @@ When a character slays another character, the victim's inventory goes to its van
 class Character {
   constructor(name, health, strength) {
     this.name = name;
-    this.health = name;
+    this.health = health;
     this.strength = strength;
     this.xp = 0;
-    // ...
+    this.gold = 10;
+    this.keys = 1;
   }
   attack(target) {
     if (this.health > 0) {
@@ -26,22 +27,46 @@ class Character {
       );
       target.health -= damage;
       if (target.health > 0) {
-        console.log(`${target.name} has ${target.heath} health points left`);
+        console.log(`${target.name} has ${target.health} health points left`);
       } else {
         target.health = 0;
         const bonusXP = 10;
         console.log(`
-        ${this.name} eliminated ${target.name} and wins ${bonusXP} experience points`);
+        ${this.name} eliminated ${target.name} and wins ${bonusXP} experience points, ${target.gold} gold, and ${target.keys} key(s) `);
         this.xp += bonusXP;
+        this.gold += target.gold;
+        this.keys += target.keys;
+        target.gold = 0;
+        target.keys = 0;
       }
     } else {
       console.log(`${this.name} can't attack because they've been eliminated`);
     }
   }
   describe() {
-    return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp} as XP points`;
+    return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp} as XP points, ${this.gold} gold, and ${this.keys} keys`;
   }
 }
 
-const myObject = new Character("Dave", 100, 50);
-myObject.method1(/* ... */);
+const aurora = new Character("Aurora", 150, 25);
+const glacius = new Character("Glacius", 130, 30);
+
+console.log("Welcome to the adventure! Here are our heroes");
+console.log(aurora.describe());
+console.log(glacius.describe());
+
+const monster = new Character("Spike", 40, 20);
+console.log("A wild monster has appeared: it's named " + monster.name);
+
+//attack Aurora
+
+console.log(monster.describe());
+
+monster.attack(aurora);
+monster.attack(glacius);
+
+aurora.attack(monster);
+glacius.attack(monster);
+
+console.log(aurora.describe());
+console.log(glacius.describe());
